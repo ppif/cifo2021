68ed62dbe6150db4fca69964d72da359d6da4055
mati <mati@DESKTOP-O208I2J>
2021-04-21 13:26:41 +0200

    primero

---
+++ ejemplo1/src/ejemplo1/test.java
@@ -0,0 +1,17 @@
+package ejemplo1;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		Scanner reader = new Scanner (System.in);
+		
+		System.out.println("Tell me your name?");
+		String name = reader.nextLine();
+		
+
+	}
+
+}
