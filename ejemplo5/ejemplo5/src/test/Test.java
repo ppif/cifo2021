c658f511492e6a50f50e47aa6f826534e2e9eb7a
mati <mati@DESKTOP-O208I2J>
2021-04-23 11:21:36 +0200

    final programa

---
+++ ejemplo5/ejemplo5/src/test/Test.java
@@ -0,0 +1,16 @@
+package test;
+import java.util.Scanner;
+
+public class Test {
+	// scope number #1
+	
+	public static void main(String[] args) {
+		// scope number #2
+		
+		System.out.println("Welcome ... loading ...");
+		
+		Operation.clientBuyProcess();
+		
+		System.out.println("finishing ....");
+	}}
+
