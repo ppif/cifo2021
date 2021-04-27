2c3894b765d5500cfb59bfa2871c86a3fddd2a53
mati <mati@DESKTOP-O208I2J>
2021-04-27 13:55:22 +0200

    crear correo......

---
+++ WordsOperations/src/wordsoperations/Editing.java
@@ -0,0 +1,36 @@
+package wordsoperations;
+
+public class Editing {
+	// scope #23
+	
+	public static void subEditing (String wordtoEdit) {
+		// scope #24
+		Operations.print("Substring from 3: ");
+		Operations.print(wordtoEdit.substring(3));
+		Operations.linebreak();
+		
+		Operations.print("Substring from 3 to 6: ");
+		Operations.print(wordtoEdit.substring(3, 6));
+		Operations.linebreak();
+		
+		Operations.print("Position @: ");
+		Operations.print(wordtoEdit.indexOf("@"));
+		Operations.linebreak();
+		
+		Operations.print("Domain: ");
+		Operations.print(wordtoEdit.substring(wordtoEdit.indexOf("@"), wordtoEdit.length()));
+		Operations.linebreak();
+		
+		Operations.print("User: ");
+		Operations.print(wordtoEdit.substring(0,wordtoEdit.indexOf("@")));
+		Operations.linebreak();
+		
+		Operations.print("New email: ");
+		Operations.print(wordtoEdit.substring(0,wordtoEdit.indexOf("@")) + "@" +  "java.com");
+		Operations.linebreak();
+		
+		
+	}
+	
+
+}
--- WordsOperations/src/wordsoperations/Init.java
+++ WordsOperations/src/wordsoperations/Init.java
@@ -3,30 +3,22 @@ package wordsoperations;
 import java.util.Scanner;
 
 public class Init {
-	private static Scanner reader;
-
-	//scope #1
+	// scope #1
 	public static void main(String[] args) {
 
-		reader = new Scanner (System.in);
-		System.out.println("Write some string ...");
-		String word = reader.nextLine();
-		
-		//Operations.print(word);
-		//Operations.linebreak();
-		Operations.printString(word);
-		//Operations.printStringH(word);
-		Operations.printStringH(word);
-		Operations.printStringV(word);
-		Operations.printStringHDash(word);
-		Operations.printStringVOffset(word);
-		Operations.printStringDiagonal(word);
-		
-		
-		//System.out.println("\nBye bye my friend...");
-		
-		
-		
+		try (// scope #2
+		Scanner reader = new Scanner(System.in)) {
+			System.out.println("Write some string ...");
+			String word = reader.nextLine();
+
+			Operations.operationsString(word);
+			Editing.subEditing (word);
+			EmailManagment.createAccount(word);
+		}
+		System.out.println("\nBye bye my friend...");
+
 	}
+
+	
 
 }
