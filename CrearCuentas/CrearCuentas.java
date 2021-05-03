cb084e6bb93fba776ee402c92085c5ad83dfd2d6
mati <mati@DESKTOP-O208I2J>
2021-05-03 12:49:22 +0200

    Crear cuentas

---
+++ CrearCuentas/CrearCuentas.java
@@ -0,0 +1,22 @@
+
+import java.util.Random;
+
+public class CrearCuentas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public static void crearUsuario(String word) {
+			
+			CrearUsuario(word);
+			CrearEmail(Word);
+			CrearPassword(Word);
+			ImpCuentUsuario(Word);
+			
+			
+			
+		}
+
+	}
+
+}
