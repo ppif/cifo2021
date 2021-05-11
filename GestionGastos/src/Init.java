ff15c4ac63eabc3738e8c25779cc58413d759a0c
mati <mati@DESKTOP-O208I2J>
2021-05-11 12:08:55 +0200

    V0,1

--- GestionGastos/src/Init.java
+++ GestionGastos/src/Init.java
@@ -5,20 +5,8 @@ public class Init {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-	Scanner reader = new Scanner(System.in);
-	
-	System.out.println("Bienvenido/a!! Introduce tu nombre:");
-	String nombre = reader.nextLine();
-	
-	
-
-		CreacionCuenta.crearCuenta("cuenta");
-		//GestionGastos.añadirGastos
-		//GestionGastos.modificarGastos
-		//GestionGastos.borrarGastos
-		//GestionGastos.listarGastos
-		//GestionGastos.salir
+		Controler.Start();
+		
 		
 	}
-
 }
